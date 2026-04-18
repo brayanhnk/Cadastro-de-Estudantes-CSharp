5b772556ec0ab0586a30f4cd86b001f087ce64bf
Brayanhnk <brayanhoinacki@gmail.com>
2026-04-18 15:18:00 -0300

    Commit inicial

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext: DbContext{
+    public DbSet<Estudante> Estudantes {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseMySql("server=localhost;database=escola;user=root;password=123",
+    ServerVersion.AutoDetect("server=localhost;database=escola;user=root;password=123"));
+}
---
+++ Models/Estudante.cs
@@ -0,0 +1,5 @@
+public class Estudante{
+    public int Id {get; set;}
+    public string Nome {get; set;}
+    public int Idade {get; set;}
+}
